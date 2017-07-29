8f369c5cae3f8199b5ff99ebb483f53dcdeef1c6
glambeth <glambeth1594@gmail.com>
2017-07-28 23:55:50 -0400

    block endpoints

--- examples.js
+++ examples.js
@@ -5,3 +5,14 @@ var network = zchain_api.network()
 network.then(function(data) {
 	console.log(data);
 });
+
+var blocks = zchain_api.getBlocks('height', 'descending', 1, 0);
+blocks.then(function(data) {
+	console.log(data);
+});
+
+var block = zchain_api.getBlock('00000003f30fb89e7a3ecd5db79acf1083ec97668b32b5a65efb2db8e09f1716');
+block.then(function(data) {
+	console.log(data);
+});
+
